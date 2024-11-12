86a0a9e8daf912345658b1532ca6bcbc5416cff6
OrMoshe <ormoshe2468@gmail.com>
2024-11-12 12:12:55 +0200

    change to get

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const postRoutes = require('./routes/gets');
+const getRoutes = require('./routes/gets');
 
 require('dotenv').config();
 const app = express();
 
 app.use(cors()); // This enables CORS for all origins. Adjust as necessary for production.
 app.use(bodyParser.json()); // This allows you to parse JSON body data.
-app.use('/', postRoutes);
+app.use('/', getRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
